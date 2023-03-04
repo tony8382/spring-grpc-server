8d91835d608c0a00f4963458155d225b05fe8dec
Li-Yu, Yang <tony830802@gmail.com>
2023-03-04 14:55:42 +0800

    complete jwt

--- src/main/java/com/lyyang/test/testgrpc/TestGrpcApplication.java
+++ src/main/java/com/lyyang/test/testgrpc/TestGrpcApplication.java
@@ -3,6 +3,7 @@ package com.lyyang.test.testgrpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 public class TestGrpcApplication {
--- src/main/java/com/lyyang/test/testgrpc/security/SecurityConfiguration.java
+++ src/main/java/com/lyyang/test/testgrpc/security/SecurityConfiguration.java
@@ -1,90 +1,66 @@
 package com.lyyang.test.testgrpc.security;
 
 
-import net.devh.boot.grpc.server.security.authentication.BasicGrpcAuthenticationReader;
+import com.lyyang.test.testgrpc.jwt.JwtGrantedAuthoritiesConverter;
+import com.lyyang.test.testgrpc.jwt.JwtProperties;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.security.authentication.BearerAuthenticationReader;
 import net.devh.boot.grpc.server.security.authentication.GrpcAuthenticationReader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.BearerTokenAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Configuration(proxyBeanMethods = false)
 @EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
 public class SecurityConfiguration {
 
-    private static final Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);
-
     @Bean
-    private PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
+    JwtAuthenticationConverter jwtAuthenticationConverter(final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter) {
+
+        final JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return converter;
     }
 
     @Bean
-    // This could be your database lookup. There are some complete implementations in spring-security-web.
-    private UserDetailsService userDetailsService(final PasswordEncoder passwordEncoder) {
-        return username -> {
-            log.info("Searching user: {}", username);
-            switch (username) {
-                case "guest": {
-                    return new User(username, passwordEncoder.encode(username + "Password"), Collections.emptyList());
-                }
-                case "user": {
-                    final List<SimpleGrantedAuthority> authorities =
-                            Arrays.asList(new SimpleGrantedAuthority("ROLE_GREET"));
-                    return new User(username, passwordEncoder.encode(username + "Password"), authorities);
-                }
-                default: {
-                    throw new UsernameNotFoundException("Could not find user!");
-                }
-            }
-        };
-    }
+    JwtAuthenticationProvider jwtAuthenticationProvider(final JwtAuthenticationConverter jwtAuthenticationConverter, final JwtDecoder jwtDecoder) {
 
-    @Bean
-        // One of your authentication providers.
-        // They ensure that the credentials are valid and populate the user's authorities.
-    DaoAuthenticationProvider daoAuthenticationProvider(
-            final UserDetailsService userDetailsService,
-            final PasswordEncoder passwordEncoder) {
-
-        final DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(passwordEncoder);
+        final JwtAuthenticationProvider provider = new JwtAuthenticationProvider(jwtDecoder);
+        provider.setJwtAuthenticationConverter(jwtAuthenticationConverter);
         return provider;
     }
 
     @Bean
-        // Add the authentication providers to the manager.
-    AuthenticationManager authenticationManager(final DaoAuthenticationProvider daoAuthenticationProvider) {
+    AuthenticationManager authenticationManager(final JwtAuthenticationProvider jwtAuthenticationProvider) {
         final List<AuthenticationProvider> providers = new ArrayList<>();
-        providers.add(daoAuthenticationProvider);
+        providers.add(jwtAuthenticationProvider);
         return new ProviderManager(providers);
     }
 
     @Bean
-        // Configure which authentication types you support.
     GrpcAuthenticationReader authenticationReader() {
-        return new BasicGrpcAuthenticationReader();
-        // final List<GrpcAuthenticationReader> readers = new ArrayList<>();
-        // readers.add(new BasicGrpcAuthenticationReader());
-        // readers.add(new SSLContextGrpcAuthenticationReader());
-        // return new CompositeGrpcAuthenticationReader(readers);
+        return new BearerAuthenticationReader(BearerTokenAuthenticationToken::new);
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(final JwtProperties jwtProperties) {
+        SecretKey originalKey = new SecretKeySpec(jwtProperties.getSignKey().getBytes(), SignatureAlgorithm.HS256.getJcaName());
+        return NimbusJwtDecoder.withSecretKey(originalKey).build();
     }
 
 }
